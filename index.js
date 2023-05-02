d19dbfe20ab388d46ee1d9e91cbc8ce68c266e7b
Peter <pschoenbauer@gmail.com>
2023-05-01 22:01:34 -0400

    dom content loaded and swtiched up id by myself

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    // document.querySelector('id')
+    console.log("The DOM has loaded");
+    
+function updateId(words){
+    let p = document.querySelector('p')
+        p.textContent = words
+}
+updateId('This is really cool!')
+
+    //console.log (document.querySelector('id'))
+  });
+
+
+
